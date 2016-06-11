6ad6aa8a756a4c1fda6e56cb0812cba216c194a8
Tikhon Jelvis <tikhon@jelv.is>
2016-06-11 11:28:12 -0700

    Bet minimum viable amount.

--- Player.hs
+++ Player.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Player where
 
 import Control.Lens
@@ -5,33 +6,55 @@ import Control.Lens
 import Data.Aeson as JS
 import Data.Aeson.Lens
 import qualified Data.List as List
+import Data.Text (unpack)
+
+import qualified Data.ByteString.Lazy as LBS
 
 
 version :: String
 version = "Default Haskell folding player"
 
 betRequest :: JS.Object -> IO Int
-betRequest gameState = return 1000
+betRequest obj = return current_buy_in
+  where Success (GameState { current_buy_in }) = fromJSON (Object obj)
 
 showdown :: JS.Object -> IO ()
 showdown gameState = return ()
 
-data GameState = GameState {players :: [Player]
+data GameState = GameState { players :: [Player]
                            , round :: Int
                            , pot :: Int
                            , small_blind :: Int
-                           , big_blind :: Int
                            , community_cards :: [Card]
-                           }
+                           , current_buy_in :: Int
+                           } deriving (Show, Eq)
+
+instance FromJSON GameState where
+  parseJSON (Object v) = GameState <$> v .: "players"
+                                   <*> v .: "round"
+                                   <*> v .: "pot"
+                                   <*> v .: "small_blind"
+                                   <*> v .: "community_cards"
+                                   <*> v .: "current_buy_in"
+  parseJSON _          = error "Not a valid game!"
 
 data Player = Player { name :: String
                      , status :: String
                      , stack :: Int
-                     }
+                     , hole_cards :: [Card]
+                     } deriving (Show, Eq)
+
+instance FromJSON Player where
+  parseJSON (Object v) = Player <$> v .: "name" <*> v .: "status" <*> v .: "stack" <*> v .: "hole_cards"
+  parseJSON _          = error "Not a valid player!"
 
 data Card = Card { rank :: Rank
-                 , suid :: Suit
-                 }
+                 , suit :: Suit
+                 } deriving (Show, Eq)
+
+instance FromJSON Card where
+  parseJSON (Object v) = Card <$> v .: "rank" <*> v .: "suit"
+  parseJSON _          = error "Not a valid card!"
 
 data Rank = N Int | K | Q | J | A deriving (Show, Eq)
 
@@ -42,7 +65,7 @@ instance FromJSON Rank where
   parseJSON (String "Q") = return Q
   parseJSON (String "K") = return K
   parseJSON (String "A") = return A
-  parseJSON (String n)   = undefined
+  parseJSON (String n)   = return . N . read $ unpack n
   parseJSON _ = error "Not a valid rank!" 
 
 instance FromJSON Suit where
